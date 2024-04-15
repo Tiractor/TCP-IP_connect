a4faff6851261a364cd181efd04576856cac84ef
Tiractor <90830536+Tiractor@users.noreply.github.com>
2024-04-15 10:48:15 +0300

    Dont lose Data

--- ICSnN/ICSnN.cpp
+++ ICSnN/ICSnN.cpp
@@ -1,4 +1,5 @@
 ﻿#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,14 +12,13 @@ void SendFile(SOCKET s, const char* filename) {
     FILE* file;
     char buffer[BUFFER_SIZE];
     size_t bytesRead;
-
+    errno_t err;
     // Откроем файл для чтения в бинарном режиме
-    file = fopen(filename, "rb");
-    if (!file) {
+    err = fopen_s(&file, filename, "rb");
+    if (err != 0 || !file) {
         perror("Failed to open file");
         return;
     }
-
     // Отправляем содержимое файла
     while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
         send(s, buffer, bytesRead, 0);
@@ -31,10 +31,11 @@ void ReceiveFile(SOCKET s, const char* filename) {
     FILE* file;
     char buffer[BUFFER_SIZE];
     int bytesReceived;
+    errno_t err;
 
-    // Откроем файл для записи в бинарном режиме
-    file = fopen(filename, "wb");
-    if (!file) {
+    // Откроем файл для записи в бинарном режиме с использованием fopen_s
+    err = fopen_s(&file, filename, "wb");
+    if (err != 0 || !file) {
         perror("Failed to open file");
         return;
     }
@@ -65,16 +66,16 @@ int main(int argc, char* argv[]) {
     }
 
     // Устанавливаем соединение с сервером
-    peer.sin_family = AF_INET;
+    /*peer.sin_family = AF_INET;
     peer.sin_port = htons(SERVER_PORT);
-    peer.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-    if (connect(s, (struct sockaddr*)&peer, sizeof(peer)) != 0) {
-        perror("Connection failed");
+    peer.sin_addr.s_addr = inet_addr(SERVER_ADDR);*/
+    if (inet_pton(AF_INET, SERVER_ADDR, &peer.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
         closesocket(s);
         WSACleanup();
         return 1;
     }
-
+    char* context = NULL;
     // Консольный интерфейс
     while (1) {
         printf("Commands:\n");
@@ -85,8 +86,8 @@ int main(int argc, char* argv[]) {
         memset(command, 0, sizeof(command));
         fgets(command, 100, stdin); // Используйте fgets в реальных программах для защиты от переполнения буфера
 
-        char* cmd = strtok(command, " ");
-        char* filename = strtok(NULL, " ");
+        char* cmd = strtok_s(command, " ", &context);
+        char* filename = strtok_s(NULL, " ", &context);
 
         if (strcmp(cmd, "send") == 0 && filename != NULL) {
             SendFile(s, filename);
